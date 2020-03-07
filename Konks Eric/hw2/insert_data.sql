8006611f3fcd5d6cad6e4472163c92a1a8bb85e1
exucutional <exdevour@gmail.com>
2020-03-07 17:38:09 +0300

    Some fixes

--- Konks Eric/hw2/insert_data.sql
+++ Konks Eric/hw2/insert_data.sql
@@ -1,8 +1,8 @@
-LOAD DATA LOCAL INFILE '/home/exucutional/Projects/database_course_2020/Konks Eric/hw2/users.txt' INTO TABLE users
-	FIELDS TERMINATED BY ',' LINES STARTING BY ';'
+LOAD DATA LOCAL INFILE '/home/exucutional/users.csv' INTO TABLE users
+	FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n';
     
-LOAD DATA LOCAL INFILE '/home/exucutional/Projects/database_course_2020/Konks Eric/hw2/payments.txt' INTO TABLe payments
-	FIELDS TERMINATED BY ',' LINES STARTING BY ';';
+LOAD DATA LOCAL INFILE '/home/exucutional/payments.csv' INTO TABLe payments
+	FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n';
     
-LOAD DATA LOCAL INFILE '/home/exucutional/Projects/database_course_2020/Konks Eric/hw2/sessions.txt' INTO TABLE sessions
-	FIELDS TERMINATED BY ',' LINES STARTING by ';';
+LOAD DATA LOCAL INFILE '/home/exucutional/sessions.csv' INTO TABLE sessions
+	FIELDS TERMINATED BY ';' LINES TERMINATED by '\n';
