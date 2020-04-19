35547be5a7a24e9073f30af48167b33a28cf0bc9
dvalentina <theviolintea@gmail.com>
2020-04-19 20:49:34 +0300

    hw4 fixes

--- Danilova Valentina/hw4/WAU.sql
+++ Danilova Valentina/hw4/WAU.sql
@@ -9,14 +9,14 @@ WITH RECURSIVE cte(dt) AS (
     FROM cte 
 	WHERE dt + INTERVAL 1 WEEK <= (SELECT MAX(CAST(sessions.begin_dttm AS DATE)) from sessions)
 )
-SELECT cte.dt, COUNT(DISTINCT sessions.user_id) AS user_count
+SELECT cte.dt, COUNT(DISTINCT sessions.user_id) AS wau
 FROM sessions RIGHT JOIN cte ON CAST(sessions.begin_dttm AS DATE) >= cte.dt 
 AND CAST(sessions.begin_dttm AS DATE) < cte.dt + INTERVAL 1 WEEK
 GROUP BY cte.dt
 ORDER BY cte.dt;
 
 -- Результат
--- # dt, user_count
+-- # dt, wau
 -- '2018-08-02', '6'
 -- '2018-08-09', '11'
 -- '2018-08-16', '17'
